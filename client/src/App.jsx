968dfcf8006257006603c2eb705229155c8955f6
Shishir <skjskjskj333@gmail.com>
2024-01-09 19:30:43 +0530

    authenticated folder added with signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import TextEditor from './components/TextEditor'
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Signup from './components/authentication/Signup';
 
 export default function App() {
   return (
     <div>
-      <TextEditor/>
+       <Router>
+        <Routes>
+        <Route path="/" element={<TextEditor/>} />
+        <Route path="/signup" element={<Signup />} />
+        </Routes>
+       </Router>
+
+    
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"],} });
 
 app.use(cors({
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3000"],
   methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
 }));
